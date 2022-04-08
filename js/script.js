0ead494fc3acf18bc6bd5f71dc7191b981995cb5
Arthur Dacenko-Boos <88643277+s1kebtz@users.noreply.github.com>
2022-04-08 20:44:48 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-let players = document.querySelectorAll('.beat__player');
-
 function currentTimeOutput(seconds) {
     if (seconds % 60 >= 10) {
         return `${Math.trunc(seconds / 60)}:${seconds % 60}`;
@@ -7,62 +5,63 @@ function currentTimeOutput(seconds) {
         return `${Math.trunc(seconds / 60)}:0${seconds % 60}`;
     }
 }
-
-players.forEach(function(player) {
-    const audio = player.querySelector('.audio');
-    const timelineInput = player.querySelector('.timeline__input');
-    const timeCurrent = player.querySelector('.time__current');
-    const timeFull = player.querySelector('.time__full');
-    const playButton = player.querySelector('.play-pause');
-
-    audio.onloadedmetadata = function() {
-        if (Math.round(audio.duration) % 60 >= 10) {
-            timeFull.innerHTML = `${Math.trunc(Math.round(audio.duration) / 60)}:${Math.round(audio.duration) % 60}`;
-        } else {
-            timeFull.innerHTML = `${Math.trunc(Math.round(audio.duration) / 60)}:0${Math.round(audio.duration) % 60}`;
+function audioInit() {
+    let players = document.querySelectorAll('.beat__player');
+    players.forEach((player) => {
+        const audio = player.querySelector('.audio');
+        const timelineInput = player.querySelector('.timeline__input');
+        const timeCurrent = player.querySelector('.time__current');
+        const timeFull = player.querySelector('.time__full');
+        const playButton = player.querySelector('.play-pause');
+        function timeFill() {
+            if (Math.round(audio.duration) % 60 >= 10) {
+                timeFull.textContent = `${Math.trunc(Math.round(audio.duration) / 60)}:${Math.round(audio.duration) % 60}`;
+            } else {
+                timeFull.textContent = `${Math.trunc(Math.round(audio.duration) / 60)}:0${Math.round(audio.duration) % 60}`;
+            }
+            timelineInput.max = Math.round(audio.duration);
         }
-        timelineInput.max = Math.round(audio.duration);
-    }
-
-    audio.addEventListener('loadedmetadata', function() {
-        timeFull.innerHTML = `${Math.trunc(Math.round(audio.duration) / 60)}:${Math.round(audio.duration) % 60}`;
-        timelineInput.max = Math.round(audio.duration);
-    })
-
-    playButton.addEventListener('click', function() {
-        if (audio.paused) {
-            playButton.setAttribute('id', 'playing');
-            audio.play();
-        } else {
-            audio.pause();
+        audio.onloadedmetadata = () => {
+            timeFill();
+        }
+        audio.addEventListener('loadedmetadata', () => {
+            timeFill();
+        })
+        audio.addEventListener('ended', () => {
             playButton.setAttribute('id', 'paused');
-        }
-        setInterval(function() {
-            timeCurrent.innerHTML = currentTimeOutput(Math.round(audio.currentTime));
-            timelineInput.value = Math.round(audio.currentTime) ;
-            if (Math.round(audio.currentTime) === Math.round(audio.duration)) {
+            audio.currentTime = 0;
+        });
+
+        playButton.addEventListener('click', () => {
+            if (audio.paused) {
+                playButton.setAttribute('id', 'playing');
+                audio.play();
+            } else {
                 audio.pause();
                 playButton.setAttribute('id', 'paused');
-                audio.currentTime = 0;
             }
-        }, 10);
-    })
-    
-    timelineInput.addEventListener('input', function() {
-        timeCurrent.innerHTML = currentTimeOutput(timelineInput.value);
-        audio.currentTime = timelineInput.value;
-    })
-});
-
-document.addEventListener('play', function(e){
-    for(let i = 0; i < players.length ; i++){
-        const playerAudio = players[i].querySelector('.audio');
-        const playerPlayButton = players[i].querySelector('.play-pause');
-        if(playerAudio != e.target){
-            playerPlayButton.setAttribute('id', 'paused');
-            playerAudio.pause();
-            playerAudio.currentTime = 0;
+            setInterval(() => {
+                timeCurrent.textContent = currentTimeOutput(Math.round(audio.currentTime));
+                timelineInput.value = Math.round(audio.currentTime) ;
+            }, 10);
+        })
+        
+        timelineInput.addEventListener('input', () => {
+            timeCurrent.textContent = currentTimeOutput(timelineInput.value);
+            audio.currentTime = timelineInput.value;
+        })
+    });
+    document.addEventListener('play', (e) => {
+        for(let i = 0; i < players.length ; i++){
+            const playerAudio = players[i].querySelector('.audio');
+            const playerPlayButton = players[i].querySelector('.play-pause');
+            if(playerAudio != e.target){
+                playerPlayButton.setAttribute('id', 'paused');
+                playerAudio.pause();
+                playerAudio.currentTime = 0;
+            }
         }
-    }
-}, true);
+    }, true);
+}
 
+document.addEventListener("DOMContentLoaded", audioInit, false);
